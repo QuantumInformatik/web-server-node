95668d8386f96d5d00c3d04eeaf8cd04ffbd0049
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-14 23:15:55 -0500

    89. servir contenido estatico

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+//servir contenido estatico
+
+app.use(express.static('public'))
+
+
 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su respectiva rutass')
 })
 
 app.get('*', (req, res) => {
-    res.send('404|Not Found')
+    res.sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(8080)
