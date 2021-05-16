945750d93980aa071fd7bd9c58a584d4e43d148e
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-15 22:28:42 -0500

    92. argumentos desde el controlador con handlebars

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', {
+        nombre: 'Juan Guillermo',
+        titulo: 'Node con parametros'
+    })
 })
 
 app.get('/hola-mundo', (req, res) => {
