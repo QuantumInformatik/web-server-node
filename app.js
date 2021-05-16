95554e6ce0573d25ce565d91ef75b2257ff4774f
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-05-15 22:12:45 -0500

    90. Servir sitio web completo

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.get('/hola-mundo', (req, res) => {
     res.send('Hola mundo en su respectiva rutass')
 })
 
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html')
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html')
+})
+
+
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/404.html')
 })
